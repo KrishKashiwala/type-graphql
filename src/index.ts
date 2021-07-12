76be43e77626886367216bdcb9ef6cd289e4dfe6
krish kashiwala <krishkashiwala@gmail.com>
2021-07-12 23:01:15 +0530

    mutations and queries configured successfully in type-graphl

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,9 @@ import {
     ObjectType,
     Field,
     ArgsType,
-    Args
+    Args,
+    Mutation,
+    Arg
 } from 'type-graphql';
 
 const users = [
@@ -75,10 +77,26 @@ class HelloResolver {
     async helloWorld() {
         return 'this is hello world strings';
     }
+    @Query(() => [User])
+    async allUsers() {
+        return await users;
+    }
     private userInfo: User[] = users;
     @Query(() => [User])
     async Users(@Args() { name }: userArgs): Promise<User[]> {
         return this.userInfo.filter((user) => user.name === name);
+    }
+    @Mutation(() => User)
+    async createUser(
+        @Arg('name') name: string,
+        @Arg('type') type: string
+    ): Promise<{}> {
+        const newUser = {
+            name: name,
+            type: type
+        };
+        users.push(newUser);
+        return { name, type };
     }
 }
 
