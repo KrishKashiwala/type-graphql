725bf1ca36ef762a0727c439ffbf3c9eb944e2bb
krish kashiwala <krishkashiwala@gmail.com>
2021-07-12 15:02:41 +0530

    apollo sandbox configured yet type-graphql need to learn more

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,99 @@
 import 'reflect-metadata';
 import { ApolloServer } from 'apollo-server-express';
-const express = require('express');
-import { buildSchema, Resolver, Query } from 'type-graphql';
+import * as Express from 'express';
+import { MaxLength, Length } from 'class-validator';
 
+import {
+    buildSchema,
+    Resolver,
+    Query,
+    ID,
+    ObjectType,
+    Field,
+    ArgsType,
+    Args
+} from 'type-graphql';
+
+const users = [
+    {
+        name: 'krish kashiwala',
+        // thoughts : [
+        //   {
+        //     left : "bad",
+        //     right : "good"
+        //   }
+        // ]
+
+        type: 'small brother'
+    },
+    {
+        name: 'vansh kashiwala',
+        // thoughts : [
+        //   {
+        //     left : "bad",
+        //     right : "good"
+        //   }
+        // ]
+        type: 'big brother'
+    }
+];
+// custom learning experience
+@ObjectType()
+class User {
+    @Field({ nullable: true })
+    name: string;
+    @Field()
+    type: string;
+    // @Field({nullable : true})
+    // thoughts : [Rate]
+}
+
+// args type object
+@ArgsType()
+class userArgs {
+    @Field({ nullable: true })
+    @MaxLength(30)
+    name: string;
+    @Field({ nullable: true })
+    @Length(30, 255)
+    type: string;
+}
+
+// @ObjectType()
+// class Rate{
+//   @Field({nullable : true})
+//   left : string
+//   @Field({nullable : true})
+//   right : string
+// }
+// console.log(ID)
+
+// query resolvers
 @Resolver()
 class HelloResolver {
-    @Query(() => String)
+    @Query(() => ID, { name: 'helloWorld', nullable: true })
     async helloWorld() {
-        return 'Hello World!';
+        return 'this is hello world strings';
     }
-    @Query(() => String)
-    async newWorld() {
-        return 'new studio world';
+    private userInfo: User[] = users;
+    @Query(() => [User])
+    async Users(@Args() { name }: userArgs): Promise<User[]> {
+        return this.userInfo.filter((user) => user.name === name);
     }
 }
 
+// mutation resolvers
+
 const main = async () => {
     const schema = await buildSchema({
-        resolvers: [HelloResolver]
+        resolvers: [HelloResolver],
+        dateScalarMode: 'timestamp'
     });
 
     const apolloServer = new ApolloServer({ schema });
-
-    const app = express();
     await apolloServer.start();
+    const app = Express();
+
     apolloServer.applyMiddleware({ app });
 
     app.listen(4000, () => {
